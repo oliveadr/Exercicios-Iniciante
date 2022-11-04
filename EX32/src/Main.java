edcb0f7e993420903aef9003b276d13d3d92d513
a_driana <72982985+oliveadr@users.noreply.github.com>
2022-11-04 13:25:52 -0300

    Exercicios resolvidos

--- EX32/src/Main.java
+++ EX32/src/Main.java
@@ -3,16 +3,33 @@
 * podem ser os lados de um triângulo.
 * Se forem, informar qual o tipo de triângulo que eles formam:
 * equilátero, isóscele ou escaleno.
-* Propriedade: o comprimento de cada lado de um triângulo
-* é menor do que a soma dos comprimentos dos outros dois lados.
+* Propriedade: o comprimento de cada lado de um triângulo é menor do que a soma dos comprimentos dos outros dois lados.
     a. Triângulo Equilátero: aquele que tem os comprimentos dos três lados iguais;
-    b. Triângulo Isóscele: aquele que tem os comprimentos de dois lados iguais.
-    c. Triângulo equilátero é também isóscele;
+    b. Triângulo Isósceles: aquele que tem os comprimentos de dois lados iguais.
+    c. Triângulo equilátero é também isósceles;
     d. Triângulo Escaleno: aquele que tem os comprimentos de seus três lados diferentes;
 */
 
+import java.util.Scanner;
+
 public class Main {
     public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int a, b, c;
+        System.out.println("informe o lado A");
+        a = sc.nextInt();
+        System.out.println("informe o lado B");
+        b = sc.nextInt();
+        System.out.println("informe o lado C");
+        c = sc.nextInt();
+
+        if(a==b && b==c){
+            System.out.println("equilatero");
+        } else if (a==b || b==c || a==c){
+            System.out.println("isosceles");
+        } else {
+            System.out.println("escaleno");
+        }
 
     }
 }
---
+++ EX33/src/Main.java
@@ -0,0 +1,15 @@
+/*
+* A escola “APRENDER” faz o pagamento de seus professores por hora/aula. Faça um algoritmo que
+* calcule e exiba o salário de um professor.
+* Sabe-se que o valor da hora/aula segue a tabela abaixo:
+    a. Professor Nível 1 R$12,00 por hora/aula;
+    b. Professor Nível 2 R$17,00 por hora/aula;
+    c. Professor Nível 3 R$25,00 por hora/aula.
+*/
+public class Main {
+    public static void main(String[] args) {
+
+
+        System.out.println("Hello world!");
+    }
+}
